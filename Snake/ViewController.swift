a243dd3714fa182db14922d041e2b73061574595
RobinChin <robinchin@kkbox.com>
2018-08-20 10:13:37 +0800

    Initial Commit

---
+++ Snake/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snake
+//
+//  Created by Robin chin on 2018/8/20.
+//  Copyright © 2018年 Robin chin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
